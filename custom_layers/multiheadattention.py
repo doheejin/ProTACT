e8f02d70372e6edf7f8ee3303d5aa38018393a33
doheejin <dohee5123@naver.com>
2023-05-18 21:38:11 +0900

    initial files

---
+++ custom_layers/multiheadattention.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, embedding_dim, num_heads=8):
+        super(MultiHeadAttention, self).__init__()
+        self.embedding_dim = embedding_dim # d_model
+        self.num_heads = num_heads
+
+        assert embedding_dim % self.num_heads == 0
+
+        self.projection_dim = embedding_dim // num_heads
+        self.query_dense = tf.keras.layers.Dense(embedding_dim)
+        self.key_dense = tf.keras.layers.Dense(embedding_dim)
+        self.value_dense = tf.keras.layers.Dense(embedding_dim)
+        self.dense = tf.keras.layers.Dense(embedding_dim)
+
+    def scaled_dot_product_attention(self, query, key, value):
+        matmul_qk = tf.matmul(query, key, transpose_b=True)
+        depth = tf.cast(tf.shape(key)[-1], tf.float32)
+        logits = matmul_qk / tf.math.sqrt(depth)
+        attention_weights = tf.nn.softmax(logits, axis=-1)
+        output = tf.matmul(attention_weights, value)
+        return output, attention_weights
+
+    def split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.projection_dim))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, inputs):
+        # x.shape = [batch_size, seq_len, embedding_dim]
+        batch_size = tf.shape(inputs)[0]
+
+        # (batch_size, seq_len, embedding_dim)
+        query = self.query_dense(inputs)
+        key = self.key_dense(inputs)
+        value = self.value_dense(inputs)
+
+        # (batch_size, num_heads, seq_len, projection_dim)
+        query = self.split_heads(query, batch_size)  
+        key = self.split_heads(key, batch_size)
+        value = self.split_heads(value, batch_size)
+
+        scaled_attention, _ = self.scaled_dot_product_attention(query, key, value)
+        # (batch_size, seq_len, num_heads, projection_dim)
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])  
+
+        # (batch_size, seq_len, embedding_dim)
+        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.embedding_dim))
+        outputs = self.dense(concat_attention)
+        #outputs = K.sum(outputs, axis=1) # 1011 추가
+        return outputs
+
