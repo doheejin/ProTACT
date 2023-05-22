ad5bcd8b9ac02d39edd9987d3930f0b13e1d283d
doheejin <dohee5123@naver.com>
2023-05-22 16:08:24 +0900

    revise feat path

--- configs/configs.py
+++ configs/configs.py
@@ -8,7 +8,7 @@ class Configs:
     EMBEDDING_PATH = 'embeddings/glove.6B.50d.txt'
     VOCAB_SIZE = 4000
     DATA_PATH = 'data/cross_prompt_attributes/'
-    FEATURES_PATH = 'data/hand_crafted_v4.csv'
+    FEATURES_PATH = 'data/hand_crafted_v3.csv'
     READABILITY_PATH = 'data/allreadability.pickle'
     PROMPT_PATH = 'data/prompt_info_pp.csv'
     EPOCHS = 50
